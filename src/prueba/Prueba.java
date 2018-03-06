2f4ccfa11a95b007950156aecc9e03191e6e7f61
user <user@com2pc32>
2018-03-06 04:49:02 -0600

    Primer commit

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,19 @@
+
+package prueba;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author user
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hola mundo");
+    }
+    
+}
